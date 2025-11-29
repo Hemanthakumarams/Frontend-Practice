0f9da06d569c50f5ba0684f8c1b322caacd0d2bd
Hemanthakumarams <162287003+Hemanthakumarams@users.noreply.github.com>
2025-11-29 16:04:21 +0530

    Add files via upload

---
+++ java script/introd.html/inter.js
@@ -0,0 +1,66 @@
+// const y=0
+// const z=false
+// console.log(y==z);
+// console.log(y===z);
+
+// console.log(NaN==NaN);
+// console.log(NaN===NaN);
+
+// let arr=[10,30,700,300,29,400,290]
+
+// let numbers=arr.map((x)=>{
+//     return x+100;
+// })
+// console.log(numbers);
+
+
+// let arr=[10,700,30,300,29,400,290]
+// let a=arr.sort()
+// console.log(a);
+
+// arr.unshift(950)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.push(870)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+
+
+// Array destructure
+// let arr=['hi','hello','namaskara']
+// let [a,b,c]=arr
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// object destructure
+// let obj={
+//     name:"hem",
+//     pno:100,
+//     age:24
+// }
+// let {name,pno,age}=obj
+// console.log(pno);
+
+
+// merge 2 strings with their corresponding charactors
+// let a="Hemanthkumar"
+// let b="hello"
+// let d=""
+// let e=Math.max(a.length,b.length)
+// for(i=0;i<=e;i++){
+//     if(i<a.length)
+//         d+=a[i]
+//     if(i<b.length)
+//         d+=b[i]
+// }
+// console.log(d);
